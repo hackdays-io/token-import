fa5b166c4acc02714f0cf4482ff5e069341e3f7f
hiroaki terayama <35390466+terapasta@users.noreply.github.com>
2022-06-02 00:15:43 +0900

    set up mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,38 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Web3Provider } from '@ethersproject/providers'
+import { Web3ReactProvider } from '@web3-react/core'
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../styles/theme';
+import createEmotionCache from '../createEmotionCache';
+import { Container } from '@mui/material';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <Container sx={{background: '#303030'}}>
+            <Component {...pageProps}/>
+          </Container>
+        </ThemeProvider>
+      </CacheProvider>
+    </Web3ReactProvider>
+  )
 }
 
 export default MyApp
